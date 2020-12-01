5c4ea17da18a9e552ca2e5a7b96054c79859d793
sumits557 <sumits557@gmail.com>
2020-12-01 07:56:57 -0500

    Adding soring boot app from the web along with some essential dependencies like actuator, thymleaf.

---
+++ initial-boot-app/src/main/java/com/boot/essentials/initialbootapp/InitialBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.boot.essentials.initialbootapp;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitialBootAppApplication.class, args);
+    }
+
+}
