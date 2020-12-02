1ca40dd76a38f5650b5d4ea9404a759f19142d97
sumits557 <sumits557@gmail.com>
2020-12-02 11:16:20 -0500

    Adding spring boot app from the web along (MVC in spring boot application). -- Added jackson dataformat xml dependencies in pom to get the response in xml form of the api. -- Introduce RestFul services in spring boot with MVC and JAX-RS

--- room-web-app/src/main/java/com/boot/webapp/roomwebapp/RoomController.java
+++ room-web-app/src/main/java/com/boot/webapp/roomwebapp/RoomController.java
@@ -1,27 +1,30 @@
 package com.boot.webapp.roomwebapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/rooms")
 public class RoomController {
-    private static List<Room> rooms = new ArrayList<>();
 
-    static {
-        for (int i=0; i<10; i++) {
-            rooms.add(new Room(i, "Room " +i, "R"+i, "Q"));
-        }
+    private RoomServices roomServices;
+
+    @Autowired
+    public RoomController(RoomServices roomServices) {
+        super();
+        this.roomServices = roomServices;
     }
 
     @GetMapping
     public  String getAllRooms(Model model) {
-        model.addAttribute("rooms", rooms);
+        model.addAttribute("rooms", this.roomServices.getAllRooms());
         return "rooms";
     }
 }
