9ef7e99d0bc59e01efa1830d46c376ad02ea34ec
sumits557 <sumits557@gmail.com>
2020-12-02 19:29:28 -0500

    Adding spring boot app from the web along (MVC in spring boot application). -- Basic Command line application with spring boot.

---
+++ room-clr-app/src/test/java/com/boot/clr/roomclrapp/RoomClrAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.clr.roomclrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomClrAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
